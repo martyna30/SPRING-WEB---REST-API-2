e0cd7beee64f60434d5fbb4127c60b709e19dcca
martyna30 <prawdzikmartyna@op.pl>
2020-05-12 11:14:03 +0200

    Trello

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -9,10 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
 
 @RestController
 @RequestMapping("/v1/trello")
